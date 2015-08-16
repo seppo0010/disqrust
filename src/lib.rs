5d2d75be268156f8956dd7af70bb72069e6025fb
Sebastian Waisbrot <seppo0010@gmail.com>
2015-08-16 12:34:44 -0700

    Simplify handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 extern crate disque;
 
-use std::collections::HashMap;
+use std::collections::HashSet;
 use std::thread::JoinHandle;
 
 use disque::Disque;
@@ -11,45 +11,43 @@ pub enum JobStatus {
     NAck,
 }
 
-pub trait JobHandler {
+pub trait Handler {
     fn process_job(&self, queue_name: &[u8], jobid: String, body: Vec<u8>) -> JobStatus;
-}
-
-pub trait ErrorHandler {
     fn process_error(&self, queue_name: &[u8], jobid: String, nack_threshold: u32, additional_deliveries_threshold: u32);
 }
 
-pub struct EventLoop<J: JobHandler, E: ErrorHandler> {
+pub struct EventLoop<H: Handler> {
     disque: Disque,
     workers: Vec<Option<JoinHandle<()>>>,
-    job_handler: HashMap<Vec<u8>, J>,
-    error_handler: HashMap<Vec<u8>, (u32, u32, E)>,
+    handler: H,
+    queues: HashSet<Vec<u8>>,
 }
 
-impl<J: JobHandler, E: ErrorHandler> EventLoop<J, E> {
-    pub fn new(disque: Disque, numworkers: usize) -> Self {
+impl<H: Handler> EventLoop<H> {
+    pub fn new(disque: Disque, numworkers: usize, handler: H) -> Self {
         let mut workers = Vec::with_capacity(numworkers);
         for _ in 0..numworkers { workers.push(None); }
         EventLoop {
             disque: disque,
             workers: workers,
-            job_handler: HashMap::new(),
-            error_handler: HashMap::new(),
+            handler: handler,
+            queues: HashSet::new(),
         }
     }
-    pub fn register_job_handler(&mut self, queue_name: Vec<u8>, job_handler: J) {
-        self.job_handler.insert(queue_name, job_handler);
+
+    pub fn watch_queue(&mut self, queue_name: Vec<u8>) {
+        self.queues.insert(queue_name);
     }
 
-    pub fn register_error_handler(&mut self, queue_name: Vec<u8>, nack_threshold: u32, additional_deliveries_threshold: u32, error_handler: E) {
-        self.error_handler.insert(queue_name, (nack_threshold, additional_deliveries_threshold, error_handler));
+    pub fn unwatch_queue(&mut self, queue_name: &Vec<u8>) {
+        self.queues.remove(queue_name);
     }
 
     fn run_once(&self) {
         let (queue, jobid, job, nack, additional_deliveries
                 ) = self.disque.getjob_withcounters(
-                    false, None, &*self.job_handler.keys().map(|k| &**k).collect::<Vec<_>>()).unwrap().unwrap();
-        self.job_handler.get(&queue).unwrap().process_job(&*queue, jobid, job);
+                    false, None, &*self.queues.iter().map(|k| &**k).collect::<Vec<_>>()).unwrap().unwrap();
+        self.handler.process_job(&*queue, jobid, job);
     }
 
     pub fn run(&self) {
